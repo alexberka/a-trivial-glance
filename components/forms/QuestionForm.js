6964922e64bdcae1bd1efda6a0bed02759fa6e1d
Alex Berka <alexanderberka@gmail.com>
2024-05-25 11:44:57 -0500

    Link/seed Firebase, establish MVP route and component files

---
+++ components/forms/QuestionForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function QuestionForm() {
+  return (
+    <div>
+      Create/Edit Question
+    </div>
+  );
+}
---
+++ pages/host/question/new.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CreateQuestion() {
+  return (
+    <div>
+      New Question Form
+    </div>
+  );
+}
