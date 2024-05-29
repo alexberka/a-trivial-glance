d9f4780bd345849e89d4849fa10af9e915de8006
Alex Berka <alexanderberka@gmail.com>
2024-05-28 20:19:16 -0500

    Build welcome page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
 // import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
-// import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
-import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import Link from 'next/link';
+import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
+// import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 
 function Home() {
-  const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
 
   // const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-    </div>
+    <>
+      <div className="welcome-buttons">
+        <div>
+          <Link passHref href="/game">
+            <button type="button">JOIN</button>
+          </Link>
+          <p>Enter As Player</p>
+        </div>
+        <div>
+          <Link passHref href="/host/game">
+            <button type="button">HOST</button>
+          </Link>
+          <p>Enter As Host</p>
+        </div>
+      </div>
+      <button type="button" onClick={signOut} className="welcome-signout">Sign Out</button>
+      <div className="welcome-title">
+        <h3>A</h3>
+        <h1>TRIVIAL</h1>
+        <h2>GLANCE</h2>
+      </div>
+    </>
   );
 }
 
