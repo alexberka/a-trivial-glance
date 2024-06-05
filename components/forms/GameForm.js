e13b06f216b429c9ee6bafda2dd8f6537c6ef3a2
Alex Berka <alexanderberka@gmail.com>
2024-06-04 20:29:03 -0500

    Initialize pages, API calls, database for games

---
+++ components/forms/GameForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function GameForm() {
+  return (
+    <div>
+      Make/Edit a Game
+    </div>
+  );
+}
---
+++ pages/host/game/[id].js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ManageGame() {
+  return (
+    <div>
+      View/Manage This Game
+    </div>
+  );
+}
